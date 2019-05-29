3b95e3c0bc88fdc865c8c7060f202858cb4afb6d
Kamikadzerek <szymekkaczmarczyk@gmail.com>
2019-05-29 15:43:46 +0200

    Add files via upload

---
+++ Asteroid/js/game.js
@@ -0,0 +1,84 @@
+window.onload = function(){
+	Game.init();
+};
+VAR = {
+	fps:60,
+	W:0,
+	H:0,
+	lastTime:0,
+
+	rand:function(min,max){
+		return Math.floor(Math.random()*(max-min+1))+min;
+	}
+}
+
+Game = {
+	init:function(){
+		Game.canvas = document.createElement('canvas');
+		Game.ctx = Game.canvas.getContext('2d');
+		Game.layout();
+
+		window.addEventListener('resize', Game.layout, false)
+
+		document.body.appendChild(Game.canvas);
+
+		Game.ship = new Ship();
+
+
+		window.addEventListener('keydown', Game.onKey, false);
+		window.addEventListener('keyup', Game.onKey, false);
+
+
+		Game.animationLoop();
+	},
+	onKey:function(event){
+		if(event.keyCode == 32 || event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 ){
+			event.preventDefault();
+			Game.key_32 = false;
+			if(event.type=='keydown' && !Game['key_'+event.keyCode]){
+				Game['key_'+event.keyCode] = true;
+				if(event.keyCode == 37){
+					Game.key_39 = false;
+				}else if(event.keyCode == 39){
+					Game.key_37 = false;
+				}else if (event.keyCode==32){
+					console.log('new bullet')
+					new Bullet();
+				}
+			}else if(event.type=='keyup'){
+				Game['key_'+event.keyCode] = false;
+			}
+		}
+
+		
+	},
+
+
+
+
+	layout:function(ev){
+		VAR.W = window.innerWidth
+		VAR.H = window.innerHeight
+
+		VAR.d = Math.min(VAR.W, VAR.H);
+
+		Game.canvas.width = VAR.W;
+		Game.canvas.height = VAR.H;
+
+		Game.ctx.fillstyle = 'white';
+		Game.ctx.strokeStyle = 'white';
+		Game.ctx.lineWidth = 3;
+		Game.ctx.lineJoin = 'round';
+	},
+	animationLoop:function(time){
+		requestAnimationFrame ( Game.animationLoop)
+		if (time - VAR.lastTime>=1000/VAR.fps){
+			VAR.lastTime = time;
+			
+			Game.ctx.clearRect(0,0,VAR.W,VAR.H)
+			//
+			Game.ship.draw();
+			Bullet.draw();
+		}
+	}
+}
